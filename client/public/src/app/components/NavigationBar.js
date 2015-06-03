399bc08ee772ad0fe36ce34015edc1d38f0cb4b4
Brian <briancjliu@gmail.com>
2015-06-03 09:06:46 -0700

    (feature)login overlay completed [closes #107]

--- client/public/src/app/components/NavigationBar.js
+++ client/public/src/app/components/NavigationBar.js
@@ -4,12 +4,13 @@ var React = require('react');
 
 var NavigationBar = React.createClass({
   render: function() {
+    var self = this;
     return (
       <div className="row NavBarContainer">
         <ul>
           <span>
             <li className="col-md-2"><a href="/">Home</a></li>
-            <li className="col-md-2"><a href="/">Dragons</a></li>
+            <li className="col-md-2"><a onClick={ self.props.showLogin }>Toggle</a></li>
           </span>
           <span className="AlignRight">
             <li className="col-md-2"><a href="/">Logout</a></li>
