3f2a640b312c8512d38de1634357c03a2db88f93
awatemonosan <mrkittyguest@Hacks-MacBook-Air.local>
2015-06-04 17:03:35 -0700

    add documentation to App.js

--- client/public/src/app/App.js
+++ client/public/src/app/App.js
@@ -43,78 +43,134 @@ var App = React.createClass({
   // Fetchers
   // These will be refactored out into FLUX eventually
 
+  //called during initialization in function componentWillMount
   fetchLessonList: function(){
+    //Make ajax request to the backend api to get the list of lessons
     $.get( "/api/lesson/",
-      (function(data){
-        this.setState({lessonList:data});
+      //pass a bound function as the callback so >this< is preserved.
+      (function( response ){
+        //Store the response in the state
+        this.setState({ lessonList: response });
       }).bind(this)
     );
   },
+  //called whenever the route changes to update section list in the current lesson
   fetchSectionList: function( lesson ){
     $.get( "/api/lesson/" +(lesson+1)+ "/section/",
-      (function(data){
-        this.setState({sectionList:data});
+      //pass a bound function as the callback so >this< is preserved.
+      (function( response ){
+        //Store the response in the state
+        this.setState({sectionList:response});
       }).bind(this)
     );
   },
 
+  //called whenever the route changes to update lessonData with the current lesson
   fetchLesson: function( lesson ){
     $.get( "/api/lesson/" +(lesson+1),
-      (function(data){
-        this.state.lessonData[ lesson ] = data;
+      //pass a bound function as the callback so >this< is preserved.
+      (function( response ){
+        //Store the response in the state
+        this.state.lessonData[ lesson ] = response;
+        //Initialize section data if necessary
         if(this.state.lessonData[ lesson ])
           this.state.lessonData[ lesson ].sectionData = [ ];
+        //save the state
         this.setState( this.state );
       }).bind(this)
     );
   },
+
+  //called whenever the route changes to update sectionData with the current section
   fetchSection: function( lesson, section ){
+    console.log('fetchSection', lesson, section);
     $.get( "/api/lesson/" +(lesson+1)+ "/section/" +(section+1),
-      (function(data){
-        if(this.state.lessonData[ lesson ])
-          this.state.lessonData[ lesson ].sectionData[ section ] = data;
-        this.setState(this.state);
+      //pass a bound function as the callback so >this< is preserved.
+      (function( response ){
+        //if the lesson has not been fetched/recieved yet
+        if(this.state.lessonData[ lesson ]){
+          //store the response in the state
+          this.state.lessonData[ lesson ].sectionData[ section ] = response;
+          this.setState(this.state);
+        } else {
+          //fetch the lesson and the section over again.
+          this.fetchLesson( lesson );
+          this.fetchSection( lesson, section );
+        }
       }).bind(this)
     );
   },
   /* == == == == == == == == == == == == == == == == */
 
+  //Called when route changes. event listener is added in componentWillMount
   routeChanged: function() {
+    // hash is an array of strings.
     var hash     = window.location.hash.split('/');
-    console.log("routeChanges", hash);
+    //the only location that is allowed to have sub sections is "lesson"
+    // if any the location in #lesson and the user is trying to access a sub section...
+    if(hash[0]!='#lesson' && hash.length>1){
+      // truncate the location down to just the root and return, triggering the routeChanged event again.
+      window.location.hash = hash[0];
+      return;
+    }
+    //if hash[2] is anything other than "section" then redirect to main page (todo: 404 page)
+    if(hash[2]!==undefined && hash[2]!=='section' && hash[2]!==""){
+      console.log("arg", hash);
+      window.location.hash = "";
+      return;
+    }
+
+    //the split array is then used to populate state
+    //the url I will be using in the following examples is:
+    //  http://TDDI.io/#lesson/1/section/2
+
+    //route is everything after "http://TDDI.io/" and before "1/section/1"
     var newState = {
-      route:          hash[0],
-      currentLesson:  parseInt(hash[1]) || 0,
-      currentSection: parseInt(hash[3]) || 0,
+      route:          hash[0], // "#lesson"
+      currentLesson:  parseInt(hash[1]) || 0, // 1
+      //hash[2] should allways be "section"
+      currentSection: parseInt(hash[3]) || 0, // 2
     };
+
+    //store the state
     this.setState(newState);
 
+    //fetch the selected lesson...
     this.fetchLesson( newState.currentLesson );
+    //... the sectionlist for that lesson...
     this.fetchSectionList( newState.currentLesson );
+    //... and the selected section in that lesson.
     this.fetchSection( newState.currentLesson, newState.currentSection );
   },
   componentWillMount: function( ) {
+    //whenever the hash changes, call routeChanged
     window.addEventListener('hashchange', this.routeChanged);
 
+    //fetch the list of lessons
     this.fetchLessonList();
 
+    //call routeChanged for the first time to initialize things
     this.routeChanged();
   },
   componentWillUnmount: function( ) {
+    //when unmounted, remove the hash listener
     window.removeEventListener('hashchange');
   },
 
+  //used to open the login overlay. Called from NavigationBar
   openLogin: function( ) {
     var overlayState = this.state.overlayState;
     this.setState({ overlayState: "block" });
     console.log("tried to open login");
   },
+  //used to close the login overlay. Called from LoginOverlay
   closeLogin: function( ) {
     var overlayState = this.state.overlayState;
     this.setState({ overlayState: "none" });
   },
   render: function( ) {
     var Child;
+    //pick a component to create based on the current route
     switch (this.state.route) {
       case '#lesson': Child = Lesson; break;
       case '#profile': Child = Profile; break;
@@ -123,8 +179,11 @@ var App = React.createClass({
 
     var sectionData = [ ];
     
+    //render might be called before the lesson and section data are fetched
+    //  if the section data for the currently selected lesson is available, use it
     if(this.state.lessonData[ this.state.currentLesson ])
       sectionData=this.state.lessonData[ this.state.currentLesson ].sectionData;
+
     return (
       <div>
         <NavigationBar 
