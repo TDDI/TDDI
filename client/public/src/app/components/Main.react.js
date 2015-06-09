92a87f5d50937e296dcbdc817a15865ed7e68a0d
Kurt Bartholomew <siever246@gmail.com>
2015-06-08 22:07:31 -0700

    (feat) Edited lesson select page text and card sizes Also fixed Navigation Bar test

--- client/public/src/app/components/Main.react.js
+++ client/public/src/app/components/Main.react.js
@@ -23,7 +23,11 @@ var Main = React.createClass({
         <a className="LessonPicture Lesson1img" href="/#lesson"></a>
         <div className="LessonTextDescription">
           <h3>Unit Testing</h3>
-          <p>In computer programming, unit testing is a software testing method by which individual units of source code, sets of one or more computer program modules together with associated control data, usage procedures, and operating procedures, are tested to determine whether they are fit for use. Intuitively, one can view a unit as the smallest testable part of an application. In procedural programming, a unit could be an entire module, but it is more commonly an individual function or procedure. In object-oriented programming, a unit is often an entire interface, such as a class, but could be an individual method. Unit tests are short code fragments created by programmers or occasionally by white box testers during the development process. It forms the basis for component testing.</p>
+          <p>
+            Learn how to test individual components in your program. 
+            Identify the most basic aspects of unit testing. 
+            Learn valuable lessons about what it means to properly test your code. 
+          </p>
         </div>
       </div>
 
@@ -31,7 +35,11 @@ var Main = React.createClass({
         <a className="LessonPicture Lesson2img "></a>
         <div className="LessonTextDescription">
           <h3>Integration Testing</h3>
-          <p>Integration testing (sometimes called integration and testing, abbreviated I&T) is the phase in software testing in which individual software modules are combined and tested as a group. It occurs after unit testing and before validation testing. Integration testing takes as its input modules that have been unit tested, groups them in larger aggregates, applies tests defined in an integration test plan to those aggregates, and delivers as its output the integrated system ready for system testing. </p>
+          <p>
+            Realize the power of multi-component testing for yourself.
+            Build on the foundations taught to you by unit testing.
+            Enable you and your team to iterate without fear. 
+          </p>
         </div>
       </div>
 
@@ -40,7 +48,7 @@ var Main = React.createClass({
         <a className="LessonPicture Lesson3img"></a>
         <div className="LessonTextDescription">        
           <h3>Visual Testing</h3>
-          <p>End-to-end testing is a methodology used to test whether the flow of an application is performing as designed from start to finish. The purpose of carrying out end-to-end tests is to identify system dependencies and to ensure that the right information is passed between various system components and systems.</p>
+          <p>Under Construction</p>
         </div>
       </div>
 
@@ -48,7 +56,7 @@ var Main = React.createClass({
         <a className="LessonPicture Lesson4img"></a>
         <div className="LessonTextDescription">        
           <h3>End to End Testing</h3>
-          <p>The aim of visual testing is to provide developers with the ability to examine what was happening at the point of software failure by presenting the data in such a way that the developer can easily ﬁnd the information she or he requires, and the information is expressed clearly. At the core of visual testing is the idea that showing someone a problem (or a test failure), rather than just describing it, greatly increases clarity and understanding. Visual testing therefore requires the recording of the entire test process – capturing everything that occurs on the test system in video format. Output videos are supplemented by real-time tester input via picture-in-a-picture webcam and audio commentary from microphones.</p>
+          <p>Under Construction</p>
         </div>
       </div>
 
