86ec3a6573af7e24e1d513f881692d09a47a9319
awatemonosan <mrkittyguest@Hacks-MacBook-Air.local>
2015-06-11 12:14:52 -0700

    adds some changes to the db data and some formatting changes

--- server/db/dbUtils.js
+++ server/db/dbUtils.js
@@ -1,3 +1,3 @@
 module.exports =  function( fn ){ 
-  return fn.toString().slice( fn.toString().indexOf('{') + 1 ).slice(0,-1);
+  return fn.toString().slice( fn.toString().indexOf('{') + 2 ).slice(0,-1);
 };
