e5340c86eb956cc2eff11691d88a1831f1d72c79
Kurt Bartholomew <siever246@gmail.com>
2015-06-18 17:40:37 -0700

    (fix) Removed 2 extra lessons that were under construction

--- client/public/src/app/components/Main.react.js
+++ client/public/src/app/components/Main.react.js
@@ -13,57 +13,37 @@ var Main = React.createClass({
   render: function() {
     return (
       <span>
-      <div className="LessonSelectContainer flex-container">
-        <div className = "LessonSelectHeader">
-          <h1>Get yourself out of Technical Debt</h1>
-          <p>Try out a lesson today</p>
-        <hr />
+        <div className="LessonSelectContainer flex-container">
+          <div className = "LessonSelectHeader">
+            <h1>Get yourself out of Technical Debt</h1>
+            <p>Try out a lesson today</p>
+          <hr />
+          </div>
         </div>
-      </div>
-      <div className="innerLessonsContainer container-fluid flex-container">
-
-      <div className="LessonArticle backglow">
-        <a className="LessonPicture Lesson1img" href="/#lesson"></a>
-        <div className="LessonTextDescription">
-          <h3>Unit Testing</h3>
-          <p>
-            Learn how to test individual components in your program. 
-            Identify the most basic aspects of unit testing. 
-            Learn valuable lessons about what it means to properly test your code. 
-          </p>
+        <div className="innerLessonsContainer container-fluid flex-container">
+          <div className="LessonArticle backglow">
+            <a className="LessonPicture Lesson1img" href="/#lesson"></a>
+            <div className="LessonTextDescription">
+              <h3>Unit Testing</h3>
+              <p>
+                Learn how to test individual components in your program. 
+                Identify the most basic aspects of unit testing. 
+                Learn valuable lessons about what it means to properly test your code. 
+              </p>
+            </div>
+          </div>
+          <div className="LessonArticle UnderConstruction">
+            <a className="LessonPicture Lesson2img "></a>
+            <div className="LessonTextDescription">
+              <h3>Integration Testing</h3>
+              <p>
+                Realize the power of multi-component testing for yourself.
+                Build on the foundations taught to you by unit testing.
+                Enable you and your team to iterate without fear. 
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="LessonArticle UnderConstruction">
-        <a className="LessonPicture Lesson2img "></a>
-        <div className="LessonTextDescription">
-          <h3>Integration Testing</h3>
-          <p>
-            Realize the power of multi-component testing for yourself.
-            Build on the foundations taught to you by unit testing.
-            Enable you and your team to iterate without fear. 
-          </p>
-        </div>
-      </div>
-
-
-      <div className="LessonArticle UnderConstruction">
-        <a className="LessonPicture Lesson3img"></a>
-        <div className="LessonTextDescription">        
-          <h3>Visual Testing</h3>
-          <p>Under Construction</p>
-        </div>
-      </div>
-
-      <div className="LessonArticle UnderConstruction">
-        <a className="LessonPicture Lesson4img"></a>
-        <div className="LessonTextDescription">        
-          <h3>End to End Testing</h3>
-          <p>Under Construction</p>
-        </div>
-      </div>
-
-      </div>
       </span>
     )
   }
